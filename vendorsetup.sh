fb3eb4be7df67d6c95ae5cda3ab18b11d85cf993
Ashik6301 <ashiksk6301@gmail.com>
2023-08-21 00:37:38 +0600

    Add vendorsetup file

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,15 +1,12 @@
-git clone https://gitlab.com/Drpto/android_vendor_MiuiCamera.git -b thirteen vendor/MiuiCamera
-git clone https://github.com/SuperiorOS-Devices/vendor_xiaomi_violet.git -b thirteen vendor/xiaomi/violet --depth=1
-git clone https://github.com/SuperiorOS-Devices/kernel_xiaomi_violet.git -b thirteen kernel/xiaomi/violet --depth=1
-rm -rf prebuilts/clang/host/linux-x86/clang-r487747
-git clone https://gitlab.com/Joker-V2/prebuilts_clang_host_linux-x86_clang-r487747.git -b master prebuilts/clang/host/linux-x86/clang-r487747 --depth=1
-git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi-firmware.git -b thirteen vendor/xiaomi-firmware --depth=1
-rm -rf packages/resources/devicesettings
-git clone https://github.com/LineageOS/android_packages_resources_devicesettings.git -b lineage-20.0 packages/resources/devicesettings --depth=1
+git clone --depth 1 https://github.com/Ashik6301/Vendor-xiaomi-violet.git -b 13.0 vendor/xiaomi/violet
+git clone --depth 1 https://github.com/Ashik6301/Kernel-violet.git -b thirteen kernel/xiaomi/violet
 rm -rf hardware/qcom-caf/sm8150/audio
+rm -rf hardware/qcom-caf/sm8150/media 
 rm -rf hardware/qcom-caf/sm8150/display
-rm -rf hardware/qcom-caf/sm8150/media
-git clone https://github.com/SuperiorOS/android_hardware_qcom_audio.git -b thirteen-caf-sm8150 hardware/qcom-caf/sm8150/audio --depth=1
-git clone https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-19.1-caf-sm8150 hardware/qcom-caf/sm8150/media --depth=1
-git clone https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-19.1-caf-sm8150 hardware/qcom-caf/sm8150/display --depth=1
+git clone --depth 1 https://github.com/SuperiorOS/android_hardware_qcom_audio.git -b thirteen-caf-sm8150 hardware/qcom-caf/sm8150/audio
+git clone --depth 1 https://github.com/LineageOS/android_hardware_qcom_media.git -b lineage-19.1-caf-sm8150 hardware/qcom-caf/sm8150/media
+git clone --depth 1 https://github.com/LineageOS/android_hardware_qcom_display.git -b lineage-20.0-caf-sm8150 hardware/qcom-caf/sm8150/display
+git clone --depth 1 https://gitlab.com/ashiksk6301/vendor_xiaomi-firmware_violet.git -b thirteen vendor/xiaomi-firmware/violet
+git clone --depth 1 https://gitlab.com/ashiksk6301/prebuilts_clang_host_linux-x86_clang-r498229.git -b master prebuilts/clang/host/linux-x86/clang-r498229
+git clone https://gitlab.com/ashiksk6301/android_vendor_miuicamera.git vendor/MiuiCamera
 
